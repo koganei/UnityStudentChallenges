0114bc29d0fbbea620db7016f7f9d1106d07a452
koganei <khoury.marc@gmail.com>
2013-12-05 22:26:08 -0500

    Stealth Game Tutorial
    This became the repo for
    https://www.assetstore.unity3d.com/#/content/7677 . The first three
    challenges are still there :o

--- Assets/Scripts/scoreOnHit.cs
+++ Assets/Scripts/scoreOnHit.cs
@@ -5,6 +5,7 @@ public class scoreOnHit : MonoBehaviour {
 	
 	void OnCollisionEnter(Collision collision) {
 		if(collision.collider.tag == "Bullet") {
+			// collision.collider.rigidbody.AddExplosionForce(200f, transform.position, 1000.0f);
 			scoreCounter.score++;
 		}
 	}
